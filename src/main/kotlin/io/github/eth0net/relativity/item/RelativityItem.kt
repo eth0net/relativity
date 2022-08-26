f5ed668bc4b1265ee4761318ee4fba1e1902e60e
Elliot Thomas <elliot@eth0net.io>
2022-08-26 09:25:02 +0100

    Add permission requirements to item

--- src/main/kotlin/io/github/eth0net/relativity/item/RelativityItem.kt
+++ src/main/kotlin/io/github/eth0net/relativity/item/RelativityItem.kt
@@ -5,6 +5,7 @@ import io.github.eth0net.relativity.Relativity.sendRateMessage
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
@@ -12,9 +13,13 @@ import net.minecraft.world.World
 class RelativityItem(settings: Settings) : Item(settings) {
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
         if (world.isClient()) return super.use(world, user, hand)
-        Relativity.tickRate = if (user.isSneaking) 100 else 0
-        user.sendRateMessage()
-        return TypedActionResult.success(user.getStackInHand(hand))
+        if (world.server?.isSingleplayer == true || user.hasPermissionLevel(4)) {
+            Relativity.tickRate = if (user.isSneaking) 100 else 0
+            user.sendRateMessage()
+            return TypedActionResult.success(user.getStackInHand(hand))
+        }
+        user.sendMessage(Text.translatable("error.relativity.permission"), true)
+        return TypedActionResult.fail(user.getStackInHand(hand))
     }
 }
 
